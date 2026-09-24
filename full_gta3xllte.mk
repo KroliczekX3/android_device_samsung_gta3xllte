e8e961253989b13ae26bcd912cf7100f7cb58d14
Króliczek X3 <kroliczek.x3@proton.me>
2026-09-24 17:59:45 +0200

    Update device configuration for gta3xllte

--- full_gta3xllte.mk
+++ full_gta3xllte.mk
@@ -4,17 +4,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/gta3xlwifi/device.mk)
+$(call inherit-product, device/samsung/gta3xllte/device.mk)
 
 # Device identifier
-PRODUCT_DEVICE := gta3xlwifi
-PRODUCT_NAME := full_gta3xlwifi
-PRODUCT_MODEL := SM-T510
+PRODUCT_DEVICE := gta3xllte
+PRODUCT_NAME := full_gta3xllte
+PRODUCT_MODEL := SM-T515
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
