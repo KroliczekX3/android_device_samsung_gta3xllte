d09546136e170c0fccba269a9adec25727d9d91b
Króliczek X3 <kroliczek.x3@proton.me>
2026-09-24 19:49:24 +0200

    Remove ARM64 architecture settings
    Removed ARM64 architecture configurations from BoardConfig.mk.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,5 @@
 DEVICE_PATH := device/samsung/gta3xllte
 
-# Architecture - ARM64
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a73
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
 # Inherit common board flags FIRST, then override
 include device/samsung/universal7904-common/BoardConfigCommon.mk
 
